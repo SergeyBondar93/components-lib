63dc8573f75a5a49285a0daed1fa40919581d7ff
Sergey Bondar <sergey.bondar@cherehapa.ru>
2022-06-14 17:45:04 +0300

    update v2
    test
    
    test: kek
    
    test: kek
    
    test: chebure

--- packages/button/stories/Button.stories.tsx
+++ packages/button/stories/Button.stories.tsx
@@ -1,14 +1,15 @@
-import { Story, Meta } from '@storybook/react';
-import { Button } from '../src';
+import { Story, Meta } from "@storybook/react";
+
+import { Button } from "../src";
 
 export default {
   component: Button,
-  title: 'Button',
+  title: "Button",
 } as Meta;
 
 const Template: Story = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  children: 'I am button!',
+  children: "I am button! V2 LOL",
 };
