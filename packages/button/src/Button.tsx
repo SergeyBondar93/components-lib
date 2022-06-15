09c09e150e495fd2f145d95c1fd2879731024263
Sergey Bondar <sergey.bondar@cherehapa.ru>
2022-06-15 12:59:59 +0300

    feat!: big change api

--- packages/button/src/Button.tsx
+++ packages/button/src/Button.tsx
@@ -3,7 +3,9 @@ import { FC } from "react";
 export const Button: FC<any> = ({ children }) => {
   return (
     <div>
-      <button type="submit">{children} lol kek cheburek my love</button>
+      <button type="submit">
+        {children} lol kek cheburek my love ee big changes
+      </button>
     </div>
   );
 };
