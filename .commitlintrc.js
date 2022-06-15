ca0db1fbf2c54acf740fb9229fc2935712525f0e
Sergey Bondar <sergey.bondar@cherehapa.ru>
2022-06-15 13:21:09 +0300

    chore(release): update version

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor']],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'chore']],
     'subject-case': [0],
     'header-max-length': [0],
     'subject-full-stop': [0],
