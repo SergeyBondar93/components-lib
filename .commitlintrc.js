e9c45bc73dce77ec7bc0ded444193ac6815fd331
Sergey Bondar <sergey.bondar@cherehapa.ru>
2022-06-15 12:53:29 +0300

    feat(button): change api BREAKING CHANGE(button): change api

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'perf', 'bump', 'ci', 'build', 'chore', 'types']],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor']],
     'subject-case': [0],
     'header-max-length': [0],
     'subject-full-stop': [0],
