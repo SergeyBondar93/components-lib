76acad90c737733ae343da53679142a9126798b4
Sergey Bondar <sergey.bondar@cherehapa.ru>
2022-08-15 13:43:12 +0300

    feat: change container paddings

--- packages/container/src/styles/defaultTheme.ts
+++ packages/container/src/styles/defaultTheme.ts
@@ -1,4 +1,4 @@
-import { ComponentTheme, BREAKPOINTS } from "@cheaaa/theme";
+import { ComponentTheme } from "@cheaaa/theme";
 
 import { CONTAINER_COMPONENTS_NAMES } from "./consts";
 import { ComponentNames } from "./types";
@@ -9,13 +9,9 @@ export const defaultTheme: Required<ComponentTheme<ComponentNames>> = {
     height: "100%",
     maxWidth: 1160,
     margin: "0 auto",
-    padding: "0 15px",
     display: "flex",
     flexDirection: "column",
     flex: "1 0 auto",
     boxSizing: "border-box",
-    [BREAKPOINTS.up("xl")]: {
-      padding: "0 14px",
-    },
   },
 };
